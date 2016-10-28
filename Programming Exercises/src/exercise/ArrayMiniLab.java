fe3ea7de722f23e4256254a093d7224f1c602e02
jomanwong <18wongc12@millshighsch.org>
2016-10-28 09:40:18 -0700

    10/28/16
    Joman Wong

---
+++ Programming Exercises/src/exercise/ArrayMiniLab.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class ArrayMiniLab {
+	public static void main(String[] args){
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1, 2, 3, 4, 5};
+	}
+	
+	public static void changeMe(int x, String str, int[] arr){
+		x += 10;
+		str += "EXTRA";
+		arr2 = arr + 1 ;
+	}
+}
