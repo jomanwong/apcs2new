33423a425bed3bfd25691d67c8aae89310b390e3
jomanwong <18wongc12@millshighsch.org>
2016-11-01 21:56:18 -0700

    11/1/16 HOME
    Joman Wong

--- StringSplit/src/Split.java
+++ StringSplit/src/Split.java
@@ -4,14 +4,14 @@ public class Split {
 
 	public static void main(String[] args){
 
-		//String.split();
-		//It's a method that acts on a string, <StringName>.split(<String sp>);
-		//Where sp is the string where the string splits
-		//And it returns an array
-		// Example: "I like apples!".split(" "); 
-		//		it will split at spaces and return an array of ["I","like","apples!"]
-		// Example 2: "I really like really red apples"split("really")
-		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
+		/*String.split();
+		It's a method that acts on a string, <StringName>.split(<String sp>);
+		Where sp is the string where the string splits
+		And it returns an array
+		 Example: "I like apples!".split(" "); 
+				it will split at spaces and return an array of ["I","like","apples!"]
+		 Example 2: "I really like really red apples"split("really")
+				it will split at the word "really" and return an array of ["I "," like "," apples!"] */
 
 		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
 
@@ -29,21 +29,36 @@ public class Split {
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		 */
 
-		String sandwich1 = "applespineapplesbreadlettustomatobaconmayohambread"; //designated place for easier location, could be simplified
+		String sandwich1 = "appletomatohamcheese"; //designated place for easier location, could be simplified
 		/** make into method, the following */
-		/*if(isOnlyBread(splittedString)){
-			System.out.println("Is only bread, you sure it's not a loaf of bread?");
-		}*/
-		splitAtBread(sandwich1);
+		System.out.println(splitAtBread(sandwich1)); //
+		
+	/** ******************************************************************************************************************************************
+	 * problem is
+	 * why does it still print out stuff, not designited, look at if:else if:else
+	 * 11/1/16 9:26 p.m.
+	 */
 	}
 
 	public static String splitAtBread(String input){
-		String inputBackUp = input;
+		if(input.length() < 5){ // no nothing
+			return "hmmm... what is a sandwich anyways?";
+		}
+		String inputBackUp = input; // could be omitted, but a backup is sometimes useful
 		input = input.toLowerCase();
+		String reversedInput = new StringBuilder(input).reverse().toString(); //reversed instance of input, memory is cheap
+		String[] inputInArr = {input};
 		input = input + " "; //this part is important, adds a space to the end, so it will still have 3 parts if ended on 2nd bread
 		String[] output = input.split("bread"); //only 2 pieces of bread, or less, input is array now
 		if(input.length() == 3){ //only 3 parts if it is 2 bread
-			return (Arrays.toString(output));
+			return output[1];
+		}else if(input.length() > 3 && input.split("bread") != inputInArr){ // more than 2 breads
+			int firstBreadLocation = input.indexOf("bread") + 5;
+			int lastBreadLocation = input.length() - reversedInput.indexOf("daerb") - 5;
+			String secondaryOut = input.substring(firstBreadLocation, lastBreadLocation);
+			return secondaryOut;
+		}else{ //1 bread
+			return "I went into a French restaraunt and asked the waiter, 'Have you got frog's legs?' He said, 'Yes,' so I said, 'Well hop into the kitchen and get me a cheese sandwich.' - Tommy Cooper";
 		}
 	}
 
