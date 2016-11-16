a2dab565cc607aa6014f74d732075e6d34a48463
jomanwong <18wongc12@millshighsch.org>
2016-11-15 21:31:21 -0800

    11/15/16 HOME

--- pikachu/src/pikachu/Pokemon.java
+++ pikachu/src/pikachu/Pokemon.java
@@ -1,12 +1,15 @@
 package pikachu;
-
+import java.util.*;
 //Made by Joman Wong, 8/29/16, APCS2
 public class Pokemon {
 	public static void main(String[]args){
-		System.out.print("Pikachu welcomes you to the world of Pokemon!");
-		//Below is the pikachu figure
-		System.out.println("\n\t\t  (\\__/)");
-		System.out.println("\t\t  (0^.^)");
-		System.out.println("\t\tz(_(\")(\")");
+		Scanner keyboard = new Scanner(System.in);
+		String line = null;
+		while(!(line = keyboard.nextLine()).isEmpty()) {
+			String[] values = line.split("\\s+");
+			System.out.print("entered: " + Arrays.toString(values) + "\n");
+		}
+		System.out.print("Bye!");
+
 	}
 }
