00ae46ad357f8a9919c102c0253f1ece0f8a00ce
jomanwong <18wongc12@millshighsch.org>
2017-03-23 09:38:31 -0700

    3/23/17 non-related code testing, recursion
    Joman Wong

--- pikachu/src/pikachu/Pokemon.java
+++ pikachu/src/pikachu/Pokemon.java
@@ -3,15 +3,21 @@ import java.util.*;
 //Made by Joman Wong, 8/29/16, APCS2
 public class Pokemon {
 	public static void main(String[]args){
-			methodOne(0);
+		System.out.println(changePi("xpix"));
 	}
 
-	public static void methodOne(int in){
-		if(in == 8){
-			System.out.print(in);
+	public static String changePi(String str){
+		if(str.length() <= 2 ){
+			if(str.equals("pi")){
+				return "3.14";
+			}else{
+				return str;
+			}
+		}else if(){
+
 		}else{
-			methodOne(in + 1);
-			System.out.print(in);
+
 		}
+
 	}
 }
