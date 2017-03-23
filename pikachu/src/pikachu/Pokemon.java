77399640c16a1df0e6edfea8233765cc53aed1e5
jomanwong <18wongc12@millshighsch.org>
2017-03-22 22:16:22 -0700

    3/22/2017 Eclipse messing' round
    Joman Wong

--- pikachu/src/pikachu/Pokemon.java
+++ pikachu/src/pikachu/Pokemon.java
@@ -3,13 +3,15 @@ import java.util.*;
 //Made by Joman Wong, 8/29/16, APCS2
 public class Pokemon {
 	public static void main(String[]args){
-		Scanner keyboard = new Scanner(System.in);
-		String line = null;
-		while(!(line = keyboard.nextLine()).isEmpty()) {
-			String[] values = line.split("\\s+");
-			System.out.print("entered: " + Arrays.toString(values) + "\n");
-		}
-		System.out.print("Bye!");
+			methodOne(0);
+	}
 
+	public static void methodOne(int in){
+		if(in == 8){
+			System.out.print(in);
+		}else{
+			methodOne(in + 1);
+			System.out.print(in);
+		}
 	}
 }
